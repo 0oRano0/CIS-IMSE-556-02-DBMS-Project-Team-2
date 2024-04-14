b35d1b8fe3d912440c7115b3a985b2c7c39769e0
0oRano0 <163891274+0oRano0@users.noreply.github.com>
2024-04-14 14:24:30 -0400

    Optimized the registration statuses

--- scripts/PopulateAlumni_AttendSectionData.sql
+++ scripts/PopulateAlumni_AttendSectionData.sql
@@ -1,4 +1,3 @@
--- Insert records for alumni users
 INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
 SELECT
     u."USER_ID",
@@ -12,24 +11,28 @@ SELECT
         WHEN RANDOM() < 0.75 THEN 3.6
         ELSE 4
     END AS "Grade",
-    'Registered' AS "Registration Status"
+    'Completed' AS "Registration Status"
 FROM
     starrs."USER" u
     CROSS JOIN (
-        SELECT DISTINCT ON ("COURSE_NO", "DEPARTMENT") "COURSE_NO", "DEPARTMENT"
-        FROM starrs."COURSE"
-    ) c
+        SELECT "COURSE_NO", "DEPARTMENT" FROM starrs."COURSE" WHERE "COURSE_NO" IN (510, 511, 5212, 513) -- Include CS510, CS511, IMSE5212, IMSE513
+        UNION ALL
+        SELECT "COURSE_NO", "DEPARTMENT" FROM (
+            SELECT "COURSE_NO", "DEPARTMENT" FROM starrs."COURSE" WHERE "COURSE_NO" NOT IN (510, 511, 5212, 513) -- Select rest of the courses
+            ORDER BY RANDOM() LIMIT 20 -- Limit total additional records to 20
+        ) AS rest_courses
+    ) AS selected_courses
     JOIN (
         SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
         FROM starrs."SECTION"
-        WHERE "YEAR" <= 2024  -- Filter out sections with year greater than 2024
-    ) s ON c."COURSE_NO" = s."COURSE_NO"
+        WHERE "YEAR" <= 2023  -- Filter out sections with year greater than 2023
+    ) s ON selected_courses."COURSE_NO" = s."COURSE_NO"
+    JOIN starrs."COURSE" c ON selected_courses."COURSE_NO" = c."COURSE_NO"
 WHERE
     u."FIRST_NAME" ILIKE '%alumni%'  -- Select alumni users
     AND NOT EXISTS (
         SELECT 1
         FROM starrs."ATTENDS_SECTION" AS att
         WHERE att."STUDENT_ID" = u."USER_ID"
-            AND att."COURSE_NO" = c."COURSE_NO"
-    )
-LIMIT 20;  -- Limit total records to 20 (10 courses x3 = 30 credits per Alumni)
+            AND att."COURSE_NO" = selected_courses."COURSE_NO"
+    );
--- scripts/PopulateStudent_AttendSectionData.sql
+++ scripts/PopulateStudent_AttendSectionData.sql
@@ -1,4 +1,4 @@
--- Null
+-- HOLD
 INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
 SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
     u."USER_ID",
@@ -7,7 +7,7 @@ SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
     s."SEMESTER",
     s."YEAR",
     NULL AS "Grade",
-    NULL AS "Registration Status"
+    'Hold' AS "Registration Status"
 FROM
     starrs."USER" u
 CROSS JOIN (
@@ -17,6 +17,7 @@ CROSS JOIN (
 JOIN (
     SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
     FROM starrs."SECTION"
+    WHERE "YEAR" > 2024  -- Filter sections after year 2024
 ) s ON c."COURSE_NO" = s."COURSE_NO"
 WHERE
     u."FIRST_NAME" ILIKE '%student%'  -- Select user IDs with "student" in their first name
@@ -26,37 +27,7 @@ WHERE
         WHERE att."STUDENT_ID" = u."USER_ID"
             AND att."COURSE_NO" = c."COURSE_NO"
     )
-LIMIT 10;
-
--- Pending
-INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
-SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
-    u."USER_ID",
-    c."COURSE_NO",
-    s."SECTION_NO",
-    s."SEMESTER",
-    s."YEAR",
-    NULL AS "Grade",
-    'Pending' AS "Registration Status"
-FROM
-    starrs."USER" u
-CROSS JOIN (
-    SELECT DISTINCT "COURSE_NO"
-    FROM starrs."COURSE"
-) c
-JOIN (
-    SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
-    FROM starrs."SECTION"
-) s ON c."COURSE_NO" = s."COURSE_NO"
-WHERE
-    u."FIRST_NAME" ILIKE '%student%'  -- Select user IDs with "student" in their first name
-    AND NOT EXISTS (
-        SELECT 1
-        FROM starrs."ATTENDS_SECTION" AS att
-        WHERE att."STUDENT_ID" = u."USER_ID"
-            AND att."COURSE_NO" = c."COURSE_NO"
-    )
-LIMIT 10;
+LIMIT 5;
 
 -- Dropped
 INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
@@ -80,6 +51,7 @@ CROSS JOIN (
 JOIN (
     SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
     FROM starrs."SECTION"
+    WHERE "YEAR" <= 2024  -- Filter sections from 2024 and prior years
 ) s ON c."COURSE_NO" = s."COURSE_NO"
 WHERE
     u."FIRST_NAME" ILIKE '%student%'  -- Select user IDs with "student" in their first name
@@ -91,7 +63,73 @@ WHERE
     )
 LIMIT 5;
 
--- Registered
+-- Null/Registered
+INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
+    u."USER_ID",
+    c."COURSE_NO",
+    s."SECTION_NO",
+    s."SEMESTER",
+    s."YEAR",
+    NULL AS "Grade",
+    'Registered' AS "Registration Status"
+FROM
+    starrs."USER" u
+CROSS JOIN (
+    SELECT DISTINCT "COURSE_NO"
+    FROM starrs."COURSE"
+) c
+JOIN (
+    SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
+    FROM starrs."SECTION"
+    WHERE "YEAR" > 2024  -- Filter sections after year 2024
+) s ON c."COURSE_NO" = s."COURSE_NO"
+WHERE
+    u."FIRST_NAME" ILIKE '%student%'  -- Select user IDs with "student" in their first name
+    AND NOT EXISTS (
+        SELECT 1
+        FROM starrs."ATTENDS_SECTION" AS att
+        WHERE att."STUDENT_ID" = u."USER_ID"
+            AND att."COURSE_NO" = c."COURSE_NO"
+    )
+LIMIT 15;
+
+
+-- Waitlist
+INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
+    u."USER_ID",
+    c."COURSE_NO",
+    s."SECTION_NO",
+    s."SEMESTER",
+    s."YEAR",
+    NULL AS "Grade",
+    'Waitlist' AS "Registration Status"
+FROM
+    starrs."USER" u
+CROSS JOIN (
+    SELECT DISTINCT "COURSE_NO"
+    FROM starrs."COURSE"
+) c
+JOIN (
+    SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
+    FROM starrs."SECTION"
+    WHERE "YEAR" > 2024  -- Filter sections after year 2024
+) s ON c."COURSE_NO" = s."COURSE_NO"
+WHERE
+    u."FIRST_NAME" ILIKE '%student%'  -- Select user IDs with "student" in their first name
+    AND NOT EXISTS (
+        SELECT 1
+        FROM starrs."ATTENDS_SECTION" AS att
+        WHERE att."STUDENT_ID" = u."USER_ID"
+            AND att."COURSE_NO" = c."COURSE_NO"
+    )
+LIMIT 10;
+
+
+
+
+-- Completed
 INSERT INTO starrs."ATTENDS_SECTION" ("STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
 SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
     u."USER_ID",
@@ -104,7 +142,7 @@ SELECT DISTINCT ON (u."USER_ID", c."COURSE_NO")
         WHEN RANDOM() < 0.7 THEN 3.3
         ELSE 2.0
     END AS "Grade",
-    'Registered' AS "Registration Status"
+    'Completed' AS "Registration Status"
 FROM
     starrs."USER" u
 CROSS JOIN (
@@ -114,6 +152,7 @@ CROSS JOIN (
 JOIN (
     SELECT DISTINCT ON ("COURSE_NO") "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR"
     FROM starrs."SECTION"
+    WHERE "YEAR" <= 2023  -- Filter sections from 2023 and prior years
 ) s ON c."COURSE_NO" = s."COURSE_NO"
 WHERE
     u."FIRST_NAME" ILIKE '%student%'  -- Select user IDs with "student" in their first name
@@ -124,3 +163,4 @@ WHERE
             AND att."COURSE_NO" = c."COURSE_NO"
     )
 LIMIT 200;
+
