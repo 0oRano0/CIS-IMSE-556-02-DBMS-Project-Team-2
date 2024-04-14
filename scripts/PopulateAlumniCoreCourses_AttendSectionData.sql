5372f1db48868498b2337c8e0a4319d0ddd0e569
0oRano0 <163891274+0oRano0@users.noreply.github.com>
2024-04-14 17:59:54 -0400

    Add files via upload

---
+++ scripts/PopulateAlumniCoreCourses_AttendSectionData.sql
@@ -0,0 +1,94 @@
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    444555, 
+    510, 
+    121, 
+    'Wi', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    444555, 
+    511, 
+    2, 
+    'Su', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    500000, 
+    511, 
+    2, 
+    'Su', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    500000, 
+    510, 
+    121, 
+    'Wi', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    500000, 
+    513, 
+    134, 
+    'Wi', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    444555, 
+    513, 
+    134, 
+    'Wi', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    444555, 
+    5212, 
+    253, 
+    'Wi', 
+    2023, 
+    4.0,
+    'Completed' 
+);
+
+INSERT INTO starrs."ATTENDS_SECTION"(
+    "STUDENT_ID", "COURSE_NO", "SECTION_NO", "SEMESTER", "YEAR", "Grade", "Registration Status")
+VALUES (
+    500000, 
+    5212, 
+    253, 
+    'Wi', 
+    2023, 
+    4.0,
+    'Completed' 
+);
